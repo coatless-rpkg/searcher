fb905da593de396976aeef7b2e3d8e293553c663
James J Balamuta <james.balamuta@gmail.com>
2018-01-15 10:37:31 -0600

    Simplify NAMESPACE for browse_url and added valid_query docs

--- R/utilities.R
+++ R/utilities.R
@@ -7,33 +7,66 @@
 #' @param encoded_query    An encoded string that satisifies [utils::URLencode()].
 #' @param open_browser     Should the URL be opened in a web browser.
 #' @inheritParams utils::browseURL
-#' @importFrom utils browseURL URLencode
+#'
+#' @return A `character` object containing the query URL
+#'
 #' @seealso [utils::browseURL()], [utils::URLencode()]
 #' @examples
-#'
-#' \dontrun{
 #' # Query Google
 #' browse_url("https://google.com/search?q=", "rstats is great")
 #'
 #' # Generate URL for Google (do not open in a web browser)
 #' browse_url("https://google.com/search?q=", "rstats is great",
 #'            open_browser = FALSE)
-#' }
+#'
+#' # Print out the hidden url
+#' print(browse_url("https://google.com/search?q=", "rstats is great",
+#'            open_browser = FALSE))
 #' @noRd
 browse_url = function(base,
                       unencoded_query, encoded_query = "",
                       browser = getOption("browser"),
                       open_browser = interactive()) {
 
-  encodedURL = paste0(base, URLencode(unencoded_query), encoded_query)
+  encodedURL = paste0(base, utils::URLencode(unencoded_query), encoded_query)
 
   if (open_browser) {
     message("Searching query in web browser ... ")
     Sys.sleep(0.5)
-    browseURL(encodedURL)
+    utils::browseURL(encodedURL)
   } else {
-    message("Please type into your browser: ", invisible(encodedURL))
+    message("Please type into your browser: \n", invisible(encodedURL))
   }
 
   invisible(encodedURL)
+}
+
+
+
+#' Validate search query
+#'
+#' Ensures that the search query is a valid string
+#'
+#' @param query Search terms to verify
+#'
+#' @return Logical value `TRUE` for okay and `FALSE` if not.
+#'
+#' @seealso [base::missing()], [base::is.null()]
+#' @examples
+#'
+#' # Valid query
+#' valid_query("rstats is great")
+#'
+#' # Not valid queries
+#' valid_query()
+#' valid_query(NULL)
+#' valid_query("")
+#' valid_query(c(1,2))
+#' @noRd
+valid_query = function(query) {
+  if(missing(query) || is.null(query) || length(query) != 1 || query == "") {
+    FALSE
+  } else {
+    TRUE
+  }
 }
